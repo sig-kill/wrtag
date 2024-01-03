71a36577b11313288127aa8166d88bc1aeb21695
sentriz <senan@senan.xyz>
2024-01-03 00:15:29 +0000

    refactor release

--- cmd/wrtag/main.go
+++ cmd/wrtag/main.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"sort"
-	"strings"
 
 	"go.senan.xyz/wrtag/musicbrainz"
+	"go.senan.xyz/wrtag/release"
 	"go.senan.xyz/wrtag/tags/tagcommon"
 	"go.senan.xyz/wrtag/tags/taglib"
+
+	"github.com/peterbourgon/ff/v4"
 )
 
 func main() {
-	flag.Parse()
+	fs := ff.NewFlagSet("wrtag")
+	_ = fs.StringLong("path-format", "", "path format")
+	_ = fs.StringLong("config", "", "config file (optional)")
+
+	userConfig, _ := os.UserConfigDir()
+	configPath := filepath.Join(userConfig, "wrtag", "config")
+
+	ffopt := []ff.Option{
+		ff.WithEnvVarPrefix("WRTAG"),
+		ff.WithConfigFileFlag("config"),
+	}
+	if stat, err := os.Stat(configPath); err == nil && stat.Mode().IsRegular() {
+		ffopt = append(ffopt,
+			ff.WithConfigFile(configPath),
+			ff.WithConfigFileParser(ff.PlainParser),
+		)
+	}
+	if err := ff.Parse(fs, os.Args[1:], ffopt...); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
 	tg := taglib.TagLib{}
 	mb := musicbrainz.NewClient()
 
-	for _, dir := range flag.Args() {
+	for _, dir := range fs.GetArgs() {
 		if err := processDir(tg, mb, dir); err != nil {
 			log.Printf("error processing dir %q: %v", dir, err)
 		}
@@ -34,68 +55,53 @@ func processDir(tg taglib.TagLib, mb *musicbrainz.Client, dir string) error {
 		return fmt.Errorf("read dir: %w", err)
 	}
 
-	var tracks []tagcommon.Info
+	var tags []tagcommon.Info
 	for _, entry := range entries {
 		if path := filepath.Join(dir, entry.Name()); tg.CanRead(path) {
 			info, err := tg.Read(path)
 			if err != nil {
 				return fmt.Errorf("read track info: %w", err)
 			}
-			tracks = append(tracks, info)
+			tags = append(tags, info)
 		}
 	}
-	if len(tracks) == 0 {
+	if len(tags) == 0 {
 		return fmt.Errorf("no tracks in dir")
 	}
-	sort.Slice(tracks, func(i, j int) bool {
-		return tracks[i].TrackNumber() < tracks[j].TrackNumber()
+	sort.Slice(tags, func(i, j int) bool {
+		return tags[i].TrackNumber() < tags[j].TrackNumber()
 	})
-	releaseInfo := tracks[0]
+	tag := tags[0]
 
 	var query musicbrainz.Query
-	query.MBReleaseID = releaseInfo.MBReleaseID()
-	query.MBArtistID = first(releaseInfo.MBArtistID())
-	query.MBReleaseGroupID = releaseInfo.MBReleaseGroupID()
-	query.Release = releaseInfo.Album()
-	query.Artist = releaseInfo.AlbumArtist()
-	query.Format = releaseInfo.Media()
-	query.Date = releaseInfo.Date()
-	query.Label = releaseInfo.Label()
-	query.CatalogueNum = releaseInfo.CatalogueNum()
-	query.NumTracks = len(tracks)
+	query.MBReleaseID = tag.MBReleaseID()
+	query.MBArtistID = first(tag.MBArtistID())
+	query.MBReleaseGroupID = tag.MBReleaseGroupID()
+	query.Release = tag.Album()
+	query.Artist = tag.AlbumArtist()
+	query.Format = tag.MediaFormat()
+	query.Date = tag.Date()
+	query.Label = tag.Label()
+	query.CatalogueNum = tag.CatalogueNum()
+	query.NumTracks = len(tags)
 
-	score, release, err := mb.SearchRelease(context.Background(), query)
+	score, resp, err := mb.SearchRelease(context.Background(), query)
 	if err != nil {
-		return fmt.Errorf("search release: %w", err)
-	}
-
-	var flatTracks []musicbrainz.Track
-	for _, ts := range release.Media {
-		flatTracks = append(flatTracks, ts.Tracks...)
-	}
-	if len(tracks) == 0 {
 		return fmt.Errorf("search release: %w", err)
 	}
 	if score < 90 {
 		return fmt.Errorf("score too low")
 	}
 
+	releaseA := release.FromTagInfo(tags)
+	releaseB := release.FromMusicBrainz(resp)
+	if len(releaseA.Tracks) != len(releaseB.Tracks) {
+		return fmt.Errorf("track count mismatch %d/%d", len(releaseA.Tracks), len(releaseB.Tracks))
+	}
+
 	fmt.Println()
 	fmt.Printf("dir: %q\n", dir)
-	fmt.Printf("score: %d\n", score)
-	fmt.Printf("release:\n")
-	fmt.Printf("  name      : %q -> %q\n", releaseInfo.Album(), release.Title)
-	fmt.Printf("  artist    : %q -> %q\n", releaseInfo.AlbumArtist(), creditString(release.ArtistCredit))
-	fmt.Printf("  label     : %q -> %q\n", releaseInfo.Label(), first(release.LabelInfo).Label.Name)
-	fmt.Printf("  catalogue : %q -> %q\n", releaseInfo.CatalogueNum(), first(release.LabelInfo).CatalogNumber)
-	fmt.Printf("  media     : %q -> %q\n", releaseInfo.Media(), release.Media[0].Format)
-	fmt.Printf("tracks:\n")
-	for i := range tracks {
-		fmt.Printf("  %02d  : %q %q\n     -> %q %q\n",
-			i,
-			tracks[i].Artist(), tracks[i].Title(),
-			creditString(flatTracks[i].ArtistCredit), flatTracks[i].Title)
-	}
+	fmt.Print(release.Diff(releaseA, releaseB))
 
 	return nil
 }
@@ -108,12 +114,4 @@ func first[T comparable](is []T) T {
 		}
 	}
 	return z
-}
-
-func creditString(artists []musicbrainz.ArtistCredit) string {
-	var sb strings.Builder
-	for _, ar := range artists {
-		fmt.Fprintf(&sb, "%s%s", ar.Name, ar.JoinPhrase)
-	}
-	return sb.String()
 }
