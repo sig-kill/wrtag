c44ef50ff55f38836b55e5d5afacdeff765ac694
sentriz <senan@senan.xyz>
2024-01-04 16:18:39 +0000

    path format

--- cmd/wrtag/main.go
+++ cmd/wrtag/main.go
@@ -8,6 +8,9 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
+	"text/template"
+	"time"
 
 	"go.senan.xyz/wrtag/musicbrainz"
 	"go.senan.xyz/wrtag/release"
@@ -19,7 +22,7 @@ import (
 
 func main() {
 	fs := ff.NewFlagSet("wrtag")
-	_ = fs.StringLong("path-format", "", "path format")
+	confPathFormat := fs.StringLong("path-format", "", "path format")
 	_ = fs.StringLong("config", "", "config file (optional)")
 
 	userConfig, _ := os.UserConfigDir()
@@ -40,17 +43,43 @@ func main() {
 		os.Exit(1)
 	}
 
+	pathFormat, err := template.
+		New("template").
+		Funcs(template.FuncMap{
+			"title": func(ar []release.Artist) []string {
+				res := make([]string, len(ar))
+				for i, v := range ar {
+					res[i] = v.Title
+				}
+				return res
+			},
+			"join": func(delim string, items []string) string {
+				return strings.Join(items, delim)
+			},
+			"year": func(t time.Time) string {
+				return fmt.Sprintf("%d", t.Year())
+			},
+			"pad0": func(amount, n int) string {
+				return fmt.Sprintf("%0*d", amount, n)
+			},
+		}).
+		Parse(*confPathFormat)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error parsing path format: %v\n", err)
+		os.Exit(1)
+	}
+
 	tg := taglib.TagLib{}
 	mb := musicbrainz.NewClient()
 
 	for _, dir := range fs.GetArgs() {
-		if err := processDir(tg, mb, dir); err != nil {
+		if err := processDir(tg, mb, pathFormat, dir); err != nil {
 			log.Printf("error processing dir %q: %v", dir, err)
 		}
 	}
 }
 
-func processDir(tg taglib.TagLib, mb *musicbrainz.Client, dir string) error {
+func processDir(tg taglib.TagLib, mb *musicbrainz.Client, pathFormat *template.Template, dir string) error {
 	entries, err := os.ReadDir(dir)
 	if err != nil {
 		return fmt.Errorf("read dir: %w", err)
@@ -115,8 +144,31 @@ func processDir(tg taglib.TagLib, mb *musicbrainz.Client, dir string) error {
 	for _, t := range files {
 		errs = append(errs, t.Close())
 	}
+	if err := errors.Join(errs...); err != nil {
+		return fmt.Errorf("write tags to files: %w", err)
+	}
 
-	return errors.Join(errs...)
+	for i, t := range releaseMB.Tracks {
+		path := filepath.Join(dir, entries[i].Name())
+		pathFormatData := struct {
+			R   *release.Release
+			T   *release.Track
+			Ext string
+		}{
+			R:   releaseMB,
+			T:   &t,
+			Ext: filepath.Ext(path),
+		}
+
+		var newPath strings.Builder
+		if err := pathFormat.Execute(&newPath, pathFormatData); err != nil {
+			return fmt.Errorf("create path: %w", err)
+		}
+
+		fmt.Println(newPath.String())
+	}
+
+	return nil
 }
 
 func first[T comparable](is []T) T {
