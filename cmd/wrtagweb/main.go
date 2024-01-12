8b7b3c473ee0a8072a8a29e0226d7b6f7104d76f
sentriz <senan@senan.xyz>
2024-01-12 00:39:23 +0000

    a

--- cmd/wrtagweb/main.go
+++ cmd/wrtagweb/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"crypto/subtle"
 	"embed"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"html/template"
@@ -11,7 +13,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/jba/muxpatterns"
 	"github.com/peterbourgon/ff/v4"
 	"go.senan.xyz/wrtag"
@@ -23,10 +27,14 @@ import (
 
 //go:embed *.html
 var ui embed.FS
+
 var templ = template.Must(
 	template.
 		New("").
 		Funcs(template.FuncMap{
+			"now": func() int64 {
+				return time.Now().UnixMilli()
+			},
 			"query": template.URLQueryEscaper,
 			"nomatch": func(err error) bool {
 				return errors.Is(err, wrtag.ErrNoMatch)
@@ -34,6 +42,8 @@ var templ = template.Must(
 		}).
 		ParseFS(ui, "*.html"),
 )
+
+var wsu websocket.Upgrader
 
 func main() {
 	ffs := ff.NewFlagSet("wrtag")
@@ -65,17 +75,38 @@ func main() {
 	jobs := map[string]*Job{} // TODO: sync
 	jobQueue := make(chan string)
 
+	wsc := map[*websocket.Conn]struct{}{}
+
+	sendJobWS := func(jobPath string) {
+		job, ok := jobs[jobPath]
+		if !ok {
+			return
+		}
+		var buff bytes.Buffer
+		if err := templ.ExecuteTemplate(&buff, "release.html", job); err != nil {
+			log.Printf("render job: %v", err)
+			return
+		}
+		for c := range wsc {
+			if err := c.WriteMessage(websocket.TextMessage, buff.Bytes()); err != nil {
+				c.Close()
+				delete(wsc, c)
+			}
+		}
+	}
+
 	for i := 0; i < 5; i++ {
 		go func() {
 			for jobPath := range jobQueue {
 				if _, ok := jobs[jobPath]; !ok {
-					jobs[jobPath] = &Job{Path: jobPath}
+					jobs[jobPath] = newJob(jobPath, "")
 				}
 				if err := processJob(context.Background(), mb, tg, jobs[jobPath]); err != nil {
 					jobs[jobPath].Error = err
 					log.Printf("error processing %q: %v", jobPath, err)
 					continue
 				}
+				sendJobWS(jobPath)
 			}
 		}()
 	}
@@ -93,18 +124,24 @@ func main() {
 		jobQueue <- path
 	})
 
-	mux.HandleFunc("POST /job/{id}", func(w http.ResponseWriter, r *http.Request) {
-		jobPath := muxpatterns.PathValue(r, "id")
-		job, ok := jobs[jobPath]
-		if !ok {
-			http.Error(w, "unknown job", http.StatusBadRequest)
+	mux.HandleFunc("GET /ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := wsu.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Printf("error upgrading: %v", err)
 			return
 		}
-		*job = Job{
-			Path:    job.Path,
-			UseMBID: r.FormValue("mbid"),
-		}
-		jobQueue <- job.Path
+		wsc[conn] = struct{}{}
+	})
+
+	mux.HandleFunc("POST /job/{id}", func(w http.ResponseWriter, r *http.Request) {
+		jobPath := decodeJobID(muxpatterns.PathValue(r, "id"))
+
+		useMBID := muxpatterns.PathValue(r, "mbid")
+		job := newJob(jobPath, useMBID)
+		jobs[jobPath] = job
+
+		jobQueue <- jobPath
+
 		if err := templ.ExecuteTemplate(w, "release.html", job); err != nil {
 			log.Printf("err in template: %v", err)
 		}
@@ -133,12 +170,17 @@ func main() {
 }
 
 type Job struct {
+	ID      string
 	Path    string
 	UseMBID string
 	Release *release.Release
 	Score   float64
 	Diff    []release.Diff
 	Error   error
+}
+
+func newJob(path string, mbid string) *Job {
+	return &Job{ID: encodeJobID(path), Path: path, UseMBID: mbid}
 }
 
 func processJob(ctx context.Context, mb *musicbrainz.Client, tg tagcommon.Reader, job *Job) (err error) {
@@ -174,4 +216,13 @@ func processJob(ctx context.Context, mb *musicbrainz.Client, tg tagcommon.Reader
 	release.ToTags(releaseMB, releaseFiles)
 
 	return nil
+}
+
+func encodeJobID(path string) string {
+	return base64.RawURLEncoding.EncodeToString([]byte(path))
+}
+
+func decodeJobID(id string) string {
+	r, _ := base64.RawURLEncoding.DecodeString(id)
+	return string(r)
 }
