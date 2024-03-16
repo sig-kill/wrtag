aee4a0fc8688c8e5be3fa52f3ad8f1e97c19b975
sentriz <senan@senan.xyz>
2024-03-16 20:02:10 +0000

    use testify assert

--- fileutil/fileutil_test.go
+++ fileutil/fileutil_test.go
@@ -3,21 +3,15 @@ package fileutil_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"go.senan.xyz/wrtag/fileutil"
 )
 
 func TestSafePath(t *testing.T) {
-	eq(t, fileutil.SafePath("hello"), "hello")
-	eq(t, fileutil.SafePath("hello/"), "hello")
-	eq(t, fileutil.SafePath("hello/a"), "hello a")
-	eq(t, fileutil.SafePath("hello / a"), "hello a")
-	eq(t, fileutil.SafePath("hel\x00lo"), "hello")
-	eq(t, fileutil.SafePath("a  b"), "a b")
-}
-
-func eq[T comparable](t *testing.T, a, b T) {
-	t.Helper()
-	if a != b {
-		t.Errorf("%v != %v", a, b)
-	}
+	assert.Equal(t, fileutil.SafePath("hello"), "hello")
+	assert.Equal(t, fileutil.SafePath("hello/"), "hello")
+	assert.Equal(t, fileutil.SafePath("hello/a"), "hello a")
+	assert.Equal(t, fileutil.SafePath("hello / a"), "hello a")
+	assert.Equal(t, fileutil.SafePath("hel\x00lo"), "hello")
+	assert.Equal(t, fileutil.SafePath("a  b"), "a b")
 }
