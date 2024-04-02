7db02340132c322ef85bed8215f11d02d2901429
sentriz <senan@senan.xyz>
2024-04-02 21:47:03 +0100

    dumpmetadata in json

--- cmd/dumpmetadata/main.go
+++ cmd/dumpmetadata/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -19,29 +20,29 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("%s\tAlbum\t%s\n", path, file.Album())
-		fmt.Printf("%s\tAlbumArtist\t%s\n", path, file.AlbumArtist())
-		fmt.Printf("%s\tAlbumArtists\t%s\n", path, file.AlbumArtists())
-		fmt.Printf("%s\tDate\t%s\n", path, file.Date())
-		fmt.Printf("%s\tOriginalDate\t%s\n", path, file.OriginalDate())
-		fmt.Printf("%s\tMediaFormat\t%s\n", path, file.MediaFormat())
-		fmt.Printf("%s\tLabel\t%s\n", path, file.Label())
-		fmt.Printf("%s\tCatalogueNum\t%s\n", path, file.CatalogueNum())
+		fmt.Printf("%s\tAlbum\t%s\n", path, format(file.Album()))
+		fmt.Printf("%s\tAlbumArtist\t%s\n", path, format(file.AlbumArtist()))
+		fmt.Printf("%s\tAlbumArtists\t%s\n", path, format(file.AlbumArtists()))
+		fmt.Printf("%s\tDate\t%s\n", path, format(file.Date()))
+		fmt.Printf("%s\tOriginalDate\t%s\n", path, format(file.OriginalDate()))
+		fmt.Printf("%s\tMediaFormat\t%s\n", path, format(file.MediaFormat()))
+		fmt.Printf("%s\tLabel\t%s\n", path, format(file.Label()))
+		fmt.Printf("%s\tCatalogueNum\t%s\n", path, format(file.CatalogueNum()))
 
-		fmt.Printf("%s\tMBReleaseID\t%s\n", path, file.MBReleaseID())
-		fmt.Printf("%s\tMBReleaseGroupID\t%s\n", path, file.MBReleaseGroupID())
-		fmt.Printf("%s\tMBAlbumArtistID\t%s\n", path, file.MBAlbumArtistID())
+		fmt.Printf("%s\tMBReleaseID\t%s\n", path, format(file.MBReleaseID()))
+		fmt.Printf("%s\tMBReleaseGroupID\t%s\n", path, format(file.MBReleaseGroupID()))
+		fmt.Printf("%s\tMBAlbumArtistID\t%s\n", path, format(file.MBAlbumArtistID()))
 
-		fmt.Printf("%s\tTitle\t%s\n", path, file.Title())
-		fmt.Printf("%s\tArtist\t%s\n", path, file.Artist())
-		fmt.Printf("%s\tArtists\t%s\n", path, file.Artists())
-		fmt.Printf("%s\tGenre\t%s\n", path, file.Genre())
-		fmt.Printf("%s\tGenres\t%s\n", path, file.Genres())
-		fmt.Printf("%s\tTrackNumber\t%v\n", path, file.TrackNumber())
-		fmt.Printf("%s\tDiscNumber\t%v\n", path, file.DiscNumber())
+		fmt.Printf("%s\tTitle\t%s\n", path, format(file.Title()))
+		fmt.Printf("%s\tArtist\t%s\n", path, format(file.Artist()))
+		fmt.Printf("%s\tArtists\t%s\n", path, format(file.Artists()))
+		fmt.Printf("%s\tGenre\t%s\n", path, format(file.Genre()))
+		fmt.Printf("%s\tGenres\t%s\n", path, format(file.Genres()))
+		fmt.Printf("%s\tTrackNumber\t%v\n", path, format(file.TrackNumber()))
+		fmt.Printf("%s\tDiscNumber\t%v\n", path, format(file.DiscNumber()))
 
-		fmt.Printf("%s\tMBRecordingID\t%s\n", path, file.MBRecordingID())
-		fmt.Printf("%s\tMBArtistID\t%s\n", path, file.MBArtistID())
+		fmt.Printf("%s\tMBRecordingID\t%s\n", path, format(file.MBRecordingID()))
+		fmt.Printf("%s\tMBArtistID\t%s\n", path, format(file.MBArtistID()))
 
 		if err := file.Close(); err != nil {
 			errs = append(errs, fmt.Errorf("close %s: %w", path, err))
@@ -53,4 +54,9 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func format(v any) string {
+	r, _ := json.Marshal(v)
+	return string(r)
 }
