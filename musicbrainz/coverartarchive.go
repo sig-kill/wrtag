4279c198817462e6f5d75f3c9e0486570787c9b6
sentriz <senan@senan.xyz>
2024-04-04 23:03:03 +0100

    context mb requests

--- musicbrainz/coverartarchive.go
+++ musicbrainz/coverartarchive.go
@@ -70,7 +70,7 @@ func (c *CAAClient) GetCoverURL(ctx context.Context, release *Release) (string, 
 }
 
 func (c *CAAClient) getCoverURL(ctx context.Context, url string) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return "", fmt.Errorf("create request: %w", err)
 	}
