e389501b003e571fb6fe31c9d3dd5a91f9f62666
sentriz <senan@senan.xyz>
2024-05-09 19:09:34 +0100

    write test files directly

--- cmd/metadata/main_test.go
+++ cmd/metadata/main_test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bytes"
 	_ "embed"
-	"io"
+	"fmt"
+	"log"
 	"os"
 	"testing"
 
@@ -13,8 +13,8 @@ import (
 func TestMain(m *testing.M) {
 	os.Exit(testscript.RunMain(m, map[string]func() int{
 		"metadata": func() int { main(); return 0 },
-		"flac":     func() int { mainFlac(); return 0 },
-		"mp3":      func() int { mainMP3(); return 0 },
+		"flac":     func() int { mainFile(); return 0 },
+		"mp3":      func() int { mainFile(); return 0 },
 	}))
 }
 
@@ -28,13 +28,20 @@ func TestScripts(t *testing.T) {
 //go:embed testdata/empty.flac
 var emptyFlac []byte
 
-func mainFlac() {
-	_, _ = io.Copy(os.Stdout, bytes.NewReader(emptyFlac))
-}
-
 //go:embed testdata/empty.mp3
 var emptyMP3 []byte
 
-func mainMP3() {
-	_, _ = io.Copy(os.Stdout, bytes.NewReader(emptyMP3))
+func mainFile() {
+	var err error
+	switch name, path := os.Args[0], os.Args[1]; name {
+	case "mp3":
+		err = os.WriteFile(path, emptyMP3, 0666)
+	case "flac":
+		err = os.WriteFile(path, emptyFlac, 0666)
+	default:
+		err = fmt.Errorf("unknown file type %q", name)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
