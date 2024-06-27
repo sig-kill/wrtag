f3420c489a26c06a578400d9aa46ac85baba0bef
sentriz <senan@senan.xyz>
2024-06-27 22:51:18 +0100

    make plugin shims their own file

--- addon/replaygain.go
+++ addon/replaygain.go
@@ -4,45 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"sync"
 
-	"go.senan.xyz/wrtag/addon/lyrics"
 	"go.senan.xyz/wrtag/addon/replaygain"
 	"go.senan.xyz/wrtag/tags"
 )
-
-type LyricsAddon struct {
-	lyrics.Source
-}
-
-func (l LyricsAddon) ProcessRelease(ctx context.Context, paths []string) error {
-	var pathErrs = make([]error, len(paths))
-	var wg sync.WaitGroup
-	for i, path := range paths {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			pathErrs[i] = tags.Write(path, func(f *tags.File) error {
-				if f.Read(tags.Lyrics) != "" {
-					return nil
-				}
-
-				lyricData, err := l.Search(ctx, f.Read(tags.ArtistCredit), f.Read(tags.Title))
-				if err != nil && !errors.Is(err, lyrics.ErrLyricsNotFound) {
-					return err
-				}
-				f.Write(tags.Lyrics, lyricData)
-				return nil
-			})
-		}()
-	}
-	wg.Wait()
-	return errors.Join(pathErrs...)
-}
-
-func (l LyricsAddon) Name() string {
-	return "lyrics"
-}
 
 type ReplayGainAddon struct {
 	TruePeak bool
